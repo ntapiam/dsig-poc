ea2ff0bc17e8c4e9f1b954373c78b268e334a779
Nikolas Tapia <tapia@wias-berlin.de>
2019-07-03 10:42:36 +0200

    updated download script

--- getdata.sh
+++ getdata.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 wget https://www.cs.ucr.edu/~eamonn/time_series_data_2018/UCRArchive_2018.zip
-unzip UCRArchive_2018 -P someone
-rm -rf UCRArchive_2018/Missing_value_and_variable_length_datasets_adjusted
+unzip -P someone UCRArchive_2018 -x */Missing_value_and_variable_length_datasets_adjusted/*
 rm UCRArchive_2018.zip
