27c544b4e899470b38b550d1db091d19346a6c6a
Nikolas Tapia <tapia@wias-berlin.de>
2020-05-07 08:41:49 +0200

    Towards PEP8 compliance

--- lib.py
+++ lib.py
@@ -1,38 +1,59 @@
-from numpy.random import ranf, randint
-from numpy import diff, cumsum, multiply, prod
-import itertools as it
-import ast
+import ast, itertools
+
+import numpy.random as rng
+import numpy as np
 
 def gen_signal(p, t, n=100, ran=10):
-    k = randint(n-ran+1)
+    k = rng.randint(n-ran+1)
     x = []
     for i in range(n):
         if i >= k and i <= (k+ran):
-            x.append(p[1]+t[1]*ranf())
+            x.append(p[1]+t[1]*rng.ranf())
         else:
-            x.append(p[0]+t[0]*ranf())
+            x.append(p[0]+t[0]*rng.ranf())
     return x
 
 def compress(x):
-    dx = list(filter(lambda v: v.any(), diff(x, axis=0)))
+    """Compresses a time series by deleting repeated values
+
+    :param x: Time series to be compressed
+    :returns: The compressed version of x
+    :rtype: list
+    """
+
+    #Compute all increments and keep only the non-zero entries
+    dx = list(filter(lambda v: v != 0, np.diff(x)))
+    #Recover the initial value
     dx.insert(0,x[0])
-    return cumsum(dx, axis=0)
+    #Return the comulative sum of the increments
+    return np.cumsum(dx)
 
 def dsig(x,L,basis=False):
-    if isinstance(x[0], (int, float)):
-        x = [[val] for val in x]
-    d = len(x[0])
-    dx = diff(compress(x), axis=0)
+    """Computes the iterated-sums signature of a time series
+
+    :param x: Time series
+    :param L: Maximum level of the signature to be computed
+    :param basis: Flag used to output basis elements together with
+        signature componentes (default=False)
+    :returns: A list of doubles representing the signature entries
+    :rtype: list
+    """
+
+    #Delete repeated entries and compute increments
+    dx = np.diff(compress(x))
+    sig = []
+    #Compositions are pre-generated and read from compositions.txt
     with open('compositions.txt', 'r') as fp:
-        comps = list(it.islice(fp, 2**L-1))
+        #This relies on the order the partitions are generated
+        comps = list(itertools.islice(fp, 2**L-1))
         for comp in comps:
             comp = ast.literal_eval(comp)
             words = it.product(*[it.combinations_with_replacement(range(0,d),p) for p in comp])
             for word in words:
                 inner = [1]*len(dx)
-                for bracket in word:
-                    outer = [prod([v[k] for k in bracket]) for v in dx]
-                    inner = cumsum(multiply(inner,outer)).tolist()
+                for p in comp:
+                    outer = [v**p for v in dx]
+                    inner = np.cumsum(np.multiply(inner,outer)).tolist()
                     last = inner.pop()
                     inner.insert(0,0)
                 if basis:
