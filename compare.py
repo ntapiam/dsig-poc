4163fc8b7f065b0b2197bf0f3fca66844d7aa1b4
Nikolas Tapia <tapia@wias-berlin.de>
2020-05-18 13:35:14 +0200

    More bugfixes

--- compare.py
+++ compare.py
@@ -5,34 +5,34 @@ parser.add_argument('results', type=argparse.FileType('r'), help='results file g
 args=parser.parse_args()
 
 with open('DataSummary.csv', 'r') as fp:
-	fp.readline()
-	benchmark = {}
-	for line in fp:
-		line = line.split(',')
-		line[8] = line[8].split(' ')[0]
-		benchmark[line[2]] = list(map(float,line[7:11]))
+    fp.readline()
+    benchmark = {}
+    for line in fp:
+        line = line.split(',')
+        line[8] = line[8].split(' ')[0]
+        benchmark[line[2]] = list(map(float,line[7:11]))
 
 improvements = {}
 args.results.readline()
 args.results.readline()
 for line in args.results:
-	if line == '\n':
-		break
-	line = line.split('\t')
-	name = line[0]
-	dsig = float(line[1])
-	improvements[name] = []
-	for rate in benchmark[name]:
-		if rate > 0:
-			improv = 1 - dsig/rate
-		elif rate == 0 and dsig > 0:
-			improv = float('-inf')
-		else:
-			improv = 0
-        improvements[name].append(f"{impov:7.4f}")
+    if line == '\n':
+        break
+    line = line.split('\t')
+    name = line[0]
+    dsig = float(line[1])
+    improvements[name] = []
+    for rate in benchmark[name]:
+        if rate > 0:
+            improv = 1 - dsig/rate
+        elif rate == 0 and dsig > 0:
+            improv = float('-inf')
+        else:
+            improv = 0
+        improvements[name].append(f"{improv:7.4f}")
 
 with open('improv.txt', 'w') as fp:
-	fp.write('Dataset EUC DTW-learn DTW default\n')
-	for name, rates in improvements.items():
-		fp.write(name + ' ' + ' '.join(map(str,rates)) + '\n')
+    fp.write('Dataset EUC DTW-learn DTW default\n')
+    for name, rates in improvements.items():
+        fp.write(name + ' ' + ' '.join(map(str,rates)) + '\n')
 
